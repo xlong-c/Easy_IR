0ad0b86d9a5d676ed3254d0134777b385abf5f91
xinglong <starcangxl@gmail.com>
2023-02-23 15:32:03 +0800

    some change

--- models/model.py
+++ models/model.py
@@ -1,25 +1,154 @@
+import os
+import time
 import torch 
 import torch.nn as nn
 from collections import OrderedDict
-
-from Easy_IR.utils.get_parts import get_model
+from torch.nn.parallel import DataParallel, DistributedDataParallel
+from Easy_IR.utils.get_parts import get_model,get_loss, get_optimizer, get_schedule
 
 
 class MODEL(nn.Module):
     def __init__(self,opts):
         self.opts = opts
         self.train_opts = opts['train']
+        self.G_opts = opts['train']
+        self.save_opts = opts['save']
+        
+        self.device = torch.device('cuda' if  self.train_opts['gpu_ids'] is not None else 'cpu')
     
+    def model_to_device(self, network):
+        network = network.to(self.device)
+        if self.train_opts['dist']:
+            find_unused_parameters = self.train_opts['find_unused_parameters']
+            use_static_graph = self.train_opts['_set_static_graph']
+            network = DistributedDataParallel(network, device_ids=[torch.cuda.current_device()], find_unused_parameters=find_unused_parameters)
+            if use_static_graph:
+                print('Using static graph. Make sure that "unused parameters" will not change during training loop.')
+                network._set_static_graph()
+        else:
+            network = DataParallel(network)
+        return network
+        
     def load(self):
-        self.model = get_model(self.train_opts['G_net']['network'],self.train_opts['G_net']['params'])
-        self.losser = get_losses(self.train_opts['G_net'])
-    
+        self.log_dict = OrderedDict()
+        self.G_model = get_model(self.G_opts['network'],self.G_opts['params'])
+        self.model_to_device(self.G_model)
+        self.G_losses = get_loss(self.G_opts['Loss_fn']['loss'],self.G_opts['Loss_fn']['weight'])
+        self.G_optimizer = get_optimizer(optim = self.G_opts['optimizer']['name'],
+                                         network = self.G_model,
+                                         opts = self.G_opts['optimizer']['param'])
+        self.G_scheduler = get_schedule(scheduler = self.G_opts['lr_scheduler']['name'], 
+                                       optimizer = self.G_optimizer, 
+                                       opts = self.G_opts['lr_scheduler'['param']])
+        if self.train_opts['E_decay'] > 0:
+            self.netE = get_model(self.G_opts['network'],self.G_opts['params']).to(self.device).eval()
+            
     def feed_data(self,sample_batch):
         self.L,self.H = sample_batch
     
     def forward_G(self):
-        self.P = self.model(self.L)
+        self.P = self.G_model(self.L)
     
+    def get_bare_model(self, network):
+        """
+        Get bare model, especially under wrapping with
+        DistributedDataParallel or DataParallel.
+        """
+        if isinstance(network, (DataParallel, DistributedDataParallel)):
+            network = network.module
+        return network
     
+    def save(self,network_label,is_best=False,epoch = 0):
+        if is_best:
+            network_label  = 'best_' + network_label
+        self.save_G(network_label,epoch)
+        
+        
+    def save_G(self,network_label,epoch):
+        network_label = 'G_net_' + network_label
+        self.save_network(self.G_model, network_label)
+        self.save_orthers(self.G_optimizer,self.G_scheduler,network_label,epoch )
+        
+    def save_network(self,network,network_label = 'G_net'):
+        save_filename = '{}.pth'.format(network_label)
+        save_path = os.path.join(self.save_opts['dir'], save_filename)
+        network = self.get_bare_model(network)
+        state_dict = network.state_dict()
+        for key, param in state_dict.items():
+            state_dict[key] = param.cpu()
+        torch.save(state_dict, save_path)
+        
+    def save_orthers(self,optimizer,scheduler, network_label = 'G_net',epoch = 0):
+        network_label += '_orthers'
+        save_filename = '{}.pth'.format(network_label)
+        save_path = os.path.join(self.save_opts['dir'], save_filename)
+        state_dict = {
+            "optimizer_state_dict": optimizer.state_dict(),
+            "schedule_state_dict": scheduler.state_dict(),
+            "save_time": time.asctime(time.localtime(time.time())),
+            'epoch': epoch
+        }
+        torch.save(state_dict, save_path)
+        
+    def load(self):
+        if self.save_opts['resume']:
+            pass
+        if self.save_opts['pretrained']:
+            self.load_network(self.G_model,self.save_opts['pretrain_path']['G_net_path'],self.save_opts['net_load_strict'])
+
+    def load_network(self,network,network_path,strict =True,param_key='params'):
+        network = self.get_bare_model(network)
+        state_dict = torch.load(network_path)
+        if param_key in state_dict.keys():
+            state_dict = state_dict[param_key]
+        network.load_state_dict(state_dict, strict=strict)
     
+    def load_orthers(self, orther_path,optimizer,scheduler):
+        state_dict = torch.load(orther_path)
+        optimizer.load_state_dict(state_dict['optimizer_state_dict'])
+        scheduler.load_state_dict(state_dict['schedule_state_dict'])
+        epoch = state_dict["epoch"]
+        save_time = state_dict['save_time']
+        print('[OK] 自{}保存的模型中加载'.format(save_time))
+        return epoch
+        
+        
+    def lossfn(self,loss,pred,target):
+        loss_detail = []
+        loss_total = 0
+        for idx, loss  in enumerate(loss['loss']):
+            ll = loss(pred,target) * loss['loss_weight'] [idx]
+            loss_detail.append(ll.item())
+            loss_total += ll
+        return loss_total, loss_detail
+    
+    def optim_parameters(self):
+        self.G_optimizer.zero_grad()
+        self.forward_G()
+        G_loss,G_loss_detail = self.lossfn(self.G_losses,self.P,self.H)      
+        G_loss.backward()
+        
+        G_optimizer_clipgrad = self.train_opts['G_optimizer_clipgrad'] if self.train_opts['G_optimizer_clipgrad'] else 0
+        if G_optimizer_clipgrad > 0:
+            torch.nn.utils.clip_grad_norm_(self.parameters(), max_norm=self.train_opts['G_optimizer_clipgrad'], norm_type=2)
+        self.G_optimizer.step()
+        
+        self.log_dict['G_loss'] = G_loss.item()
+        self.log_dict['G_loss_detail'] = G_loss_detail
+        self.log_dict['G_lr'] = self.G_scheduler.get_last_lr()[0]
+        if not self.train_opts['lr_update_per_step'] :
+            self.G_scheduler.step()  # 按步数来更行学习率
+        
+        if self.train_opts['E_decay'] > 0:
+            self.update_E(self.train_opts['E_decay'])
+            
+    def get_res (self):
+        return self.L,self.P,self.H
+    
+    def get_log_dict(self):
+        return self.log_dict
+    
+
+                    
+        
     
--- utils/get_parts.py
+++ utils/get_parts.py
@@ -17,8 +17,24 @@ def get_loss_(loss):
         return importlib.import_module('Loss_Metrics', loss['name'])()
 
 def get_loss(loss_name:list,loss_weight:list):
+    assert len(loss_name) == len(loss_weight)
     losses = []
     for loss in loss_name:
         losses.append(get_loss_(loss['name']))
         print(f'[OK] 损失函数{loss} 启用')
     return {'loss': losses, 'loss_weight': loss_weight, 'lose_name': loss_name}
+
+def get_optimizer(optim, network, opts):
+    optim_params = []
+    for k, v in network.named_parameters():
+        if v.requires_grad:
+            optim_params.append(v)
+        else:
+            print('Params [{:s}] will not optimize.'.format(k))
+    optimizer = importlib.import_module('torch.optim', optim)
+    return optimizer(optim_params, opts)
+
+def get_schedule(scheduler, optimizer, opts):
+    sch = importlib.import_module('torch.optim.lr_scheduler', scheduler)
+
+    return sch(optimizer, opts)
